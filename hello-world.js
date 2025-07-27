1e5c994ff1a2b3b5c44208e5ffd0b2e54e360dab
sagar <sagarrajput2111@gmail.com>
2025-07-27 22:24:37 +0530

    First commit

---
+++ hello-world.js
@@ -0,0 +1,66 @@
+
+//1. Print 
+//2. Interpreted language---vs compiled language
+//3. Datatypes Primitives
+
+//number
+//string
+//symbol
+//null
+//undefined
+//Bigint
+//boolean
+
+//var keyword
+//let and const
+//diff b/w var and let
+
+//loops== for  while 
+//if else
+
+//hw
+// odd even using for loop 100
+//  String reverse == using two pointer---    str=  sagar==5
+// find prime number 
+// palindrome
+// armstrong
+
+// i=0  j=str.length-1
+
+// i=0 j=4
+// ragas
+// 1,3
+// ragas
+// 22
+// ragas
+// 3 1
+
+// while(i<=j){
+//     Swap(str[i],str[j]);
+//     i++;
+//     j--;
+// }
+
+
+
+
+
+
+
+
+
+
+// let x=1;
+
+// {
+//     console.log(x+" inside block")
+// }
+
+
+
+// console.log(x+" outside block");
+
+
+
+
+
