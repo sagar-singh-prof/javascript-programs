5c6459eec1f21397a4275ec6a97198e3a7c5d06c
sagar <sagarrajput2111@gmail.com>
2025-07-28 22:13:49 +0530

    Day 2

---
+++ day-2/arrays.js
@@ -0,0 +1,30 @@
+// let arr=[1,2,3,4,6];
+
+// let arr1=["sa","sb"];
+
+// arr[0];
+// arr[1];
+
+
+// let a=[];
+// a.push(1);
+// a.push(2);
+// a.push(1);
+// a.push(2);
+
+// console.log(a.length);
+// a.pop();
+// console.log(a.length);
+
+//hw
+//  push/pop===  5 function of arrays inbuilt most used  ==   shift() slice()     use 
+// [1,2,5,6,7]sum of values in array
+// reverse of reverse
+// given a array[2,3,5]=== print squares of these arrays==[4,9,25];=== create a new array and use push function
+//string most used 5 inbuilt function==use 
+
+
+//object
+let obj={"name":"yashika","surname":"joshi","age":20};
+console.log(obj["name"])
+// console.log(obj.age+obj.name+obj.surname);
