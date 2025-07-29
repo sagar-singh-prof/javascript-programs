7f2d51602e2c8c77d6eb6e53ee45aaf2603af61d
Yashika Joshi <yashika.joshi.prof@gmail.com>
2025-07-29 22:07:38 +0530

    day2 - square of array completed

--- day-2/sqarray.js
+++ day-2/sqarray.js
@@ -1,7 +1,15 @@
-let arr = [1,2,3,4];
+// let arr = [1,2,3,4];
 
-let square = arr.map(num => num * num);
+// let square = arr.map(num => num * num);
 
-console.log("original", arr);  
+// console.log("original", arr);  
 
-console.log("Squared", square);   
+// console.log("Squared", square);   //isme push kese use krenge vo ni aaya mko kha p lgau push
+
+
+let arr =[1,2,3,4];
+let squ =[];
+ for(let i=0;i<arr.length;i++){
+ squ.push(arr[i]*arr[i]);
+ }
+ console.log("square",squ);
